c3cfb0bd85b67c33d033c7878ec1b8d3f7338e2b
voltae <ic17b096@technikum-wien.at>
2018-11-20 08:10:38 +0100

    Function create socket in client
    Signed-off-by: voltae <ic17b096@technikum-wien.at>

--- Client.c
+++ Client.c
@@ -18,27 +18,35 @@
 #include <errno.h>          // provides errno
 #include <stdio.h>          // provides the printf()
 #include <stdlib.h>         // provides exit(), EXIT_FAILURE
+#include <string.h>         // provide strerror(), strlen()
 
 void usage(char* message, char* progname);
-void errorMessage (char *message, char* progname);
+
+void errorMessage(char* userMessage, char* errorMessage, char* progname);
 
 int main (int argc, char* argv[]) {
-    int fd_socket;
+    int fd_socket, connect;
     // progname
     char* progname = argv[0];
 
     // Create a socket, return value is a file descriptor
     fd_socket = socket(AF_INET, SOCK_STREAM, 0);
     //
-    if (fd_socket < 0)
-    {
-        errorMessage("Could not create a socket", progname);
+    if (fd_socket < 0) {
+        errorMessage("Could not create a socket: ", strerror(errno), progname);
     }
-
 }
 
-void errorMessage (char *message, char* progname) {
-    fprintf(stderr, "%s %s", progname, message);
+    // try to connect to the server
+
+/**
+ * Print out the error message for th user
+ * @param userMessage message appropriate for the user
+ * @param errorMessage message from the system
+ * @param progname provided from the args
+ */
+    void errorMessage(char* userMessage, char* errorMessage, char* progname) {
+        fprintf(stderr, "%s: %s %s", progname, userMessage, errorMessage);
     exit(EXIT_FAILURE);
 }
 
