f7edd992d8df6ab12fdf1ffb977eebccfba9b164
voltae <ic17b096@technikum-wien.at>
2018-11-26 17:32:05 +0100

    Ergänzungen im Fragenkatalog.

---
+++ common.c
@@ -0,0 +1,4 @@
+//
+// Created by marcaurel on 26.11.18.
+//
+
---
+++ header.h
@@ -0,0 +1,8 @@
+//
+// Created by marcaurel on 26.11.18.
+//
+
+#ifndef SERVERCLIENT_C_HEADER_H
+#define SERVERCLIENT_C_HEADER_H
+
+#endif //SERVERCLIENT_C_HEADER_H
