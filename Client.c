59b0421a0e65be34aa35b4b5bfcee8d04eb1b192
voltae <ic17b096@technikum-wien.at>
2018-11-26 12:17:12 +0100

    Created a first working version of the client/ server Application. The client-server connection get established and both exchange message and print them to stdout.
    Signed-off-by: voltae <ic17b096@technikum-wien.at>

--- Client.c
+++ Client.c
@@ -19,6 +19,13 @@
 #include <stdio.h>          // provides the printf()
 #include <stdlib.h>         // provides exit(), EXIT_FAILURE
 #include <string.h>         // provide strerror(), strlen()
+#include <arpa/inet.h>      // provides inet_address
+#include <unistd.h>         // provides read(), write(), close()
+
+/*@TODO Replace testing addresses with user defined from args */
+#define ADDRESS "127.0.0.1"
+#define PORT 7329
+#define RECEIVERBUFFER 100
 
 /**
  * @brief is called upon failure and displays a userinfor to stderr and terminates afterwards.
@@ -28,27 +35,76 @@
                    contents of argv[0]).
  * @param exitcode the  exit code to be used in the call to exit(3) for termi-
                    nating the program.
-
  */
-void usage(FILE* stream, const char* cmnd, int exitcode);
+static void usage(FILE* stream, const char* cmnd, int exitcode);
 
-void errorMessage(char* userMessage, char* errorMessage, char* progname);
+static void errorMessage(char* userMessage, char* errorMessage, char* progname);
 
-int main (int argc, char* argv[]) {
-    int fd_socket, connect;
-    // progname
-    char* progname = argv[0];
+int main(int argc, char* argv[]) {
+    int fd_socket;                   // file descriptor client socket
+    struct sockaddr_in server_add;   // struct containing server address and port
+    char* progname = argv[0];        // Program name for error output
+    const char testmessage[] = "This is a test to the server\0";
+    ssize_t message_length = strlen(testmessage);
+    char receiveBuffer[RECEIVERBUFFER] = {"\0"}; // Receive Buffer
+    int i;                      // Counter for message
 
-    // Create a socket, return value is a file descriptor
+    // Create a SOCKET(), return value is a file descriptor
     fd_socket = socket(AF_INET, SOCK_STREAM, 0);
     //
     if (fd_socket < 0) {
         errorMessage("Could not create a socket: ", strerror(errno), progname);
     }
+    fprintf(stdout, "Client Socket created.\n");
 
+
+    // initialize struct with 0
+    memset(&server_add, 0, sizeof(server_add));
+    /* Convert Internet host address from numbers-and-dots notation in CP
+   into binary data in network byte order.  */
+    server_add.sin_addr.s_addr = inet_addr(ADDRESS);
+    server_add.sin_family = AF_INET;
+    server_add.sin_port = htons(PORT);
+
+    // try to CONNECT() to the server
+    int success = connect(fd_socket, (struct sockaddr*) &server_add, sizeof(server_add));
+    if (success < 0) {
+        errorMessage("Could not connect to a Server:", strerror(errno), progname);
+    }
+    /* inet_ntoa: Convert Internet number in IN to ASCII representation.  The return value
+   is a pointer to an internal array containing the string.*/
+    fprintf(stdout, "... Connection to Server: %s:%d established\n",
+            inet_ntoa(server_add.sin_addr), ntohs(server_add.sin_port));
+
+    /* Here begins the write read loop of the client */
+    ssize_t sendbytes, recbytes;
+    write(fd_socket, testmessage, sizeof(testmessage));
+    if (sendbytes < 0) {
+        errorMessage("Could not write to server: ", strerror(errno), progname);
+    }
+
+    /* Close the write connection from the client, nothing to say ... */
+    if (shutdown(fd_socket, SHUT_WR) < 0) {
+        errorMessage("Could not close the WR socket: ", strerror(errno), progname);
+    }
+
+    recbytes = read(fd_socket, receiveBuffer, RECEIVERBUFFER);
+
+    if (recbytes < 0) {
+        errorMessage("Could not read from server: ", strerror(errno), progname);
+    }
+    fprintf(stdout, "Server says: %s\n", receiveBuffer);
+    /* Close the read connection from the client, over and out ... */
+    if (shutdown(fd_socket, SHUT_RDWR) < 0) {
+        errorMessage("Could not close the RD socket: ", strerror(errno), progname);
+    }
+    // print out the received buffer
+    fprintf(stdout, "Server says: %\n", receiveBuffer);
+    // CLOSE()
+    if (close(fd_socket) < 0) {
+        errorMessage("Error in closing socket", strerror(errno), progname);
+    }
 }
-
-    // try to connect to the server
 
 /**
  * Print out the error message for th user
@@ -56,8 +112,8 @@ int main (int argc, char* argv[]) {
  * @param errorMessage message from the system
  * @param progname provided from the args
  */
-    void errorMessage(char* userMessage, char* errorMessage, char* progname) {
-        fprintf(stderr, "%s: %s %s", progname, userMessage, errorMessage);
+static void errorMessage(char* userMessage, char* errorMessage, char* progname) {
+    fprintf(stderr, "%s: %s %s", progname, userMessage, errorMessage);
     exit(EXIT_FAILURE);
 }
 
--- Server.c
+++ Server.c
@@ -5,13 +5,96 @@
  * @brief Implementation of a simple server Application to work with a client
  * TCP/IP Lecture Distributed Systems
  */
+#include <sys/types.h>
+#include <sys/socket.h>     // provides socket
+#include <errno.h>          // provides errno
+#include <stdio.h>          // provides the printf()
+#include <stdlib.h>         // provides exit(), EXIT_FAILURE
+#include <string.h>         // provide strerror(), strlen()
+#include <arpa/inet.h>      // provides inet_address
+#include <unistd.h>         // provides read(), write(), close()
 
+/*@TODO Replace testing addresses with user defined from args */
+#define ADDRESS "127.0.0.1"
+#define PORT 7329
+#define RECEIVERBUFFER 100
 /* Server needs following syscalls
- * socket()
- * bind()
- * listen()
+ * socket() x
+ * bind()   x
+ * listen() x
  * accept()
  * read()
  * write()
  * close()
  */
+/*@TODO Replace testing addresses with user defined from args */
+#define ADDRESS "127.0.0.1"
+#define PORT 7329
+#define RECEIVERBUFFER 100
+#define BACKLOG 5
+
+static void errorMessage(char* userMessage, char* errorMessage, const char* progname);
+
+int main(int argc, const char** argv) {
+    char buffer[RECEIVERBUFFER] = {"\0"};
+    char messageServer[] = "The Server is saying annoying things.";
+    // file descriptor socket server
+    int fd_socket_server, fd_socket_client;
+    struct sockaddr_in server_add, client_add;  // Server Socket, Client Socket
+    const char* prog = argv[0];
+    // SOCKET()
+    fd_socket_server = socket(AF_INET, SOCK_STREAM, 0);
+    if (fd_socket_server < 0) {
+        errorMessage("Could not create a socket: ", strerror(errno), prog);
+    }
+
+    // initialize struct with 0
+    memset(&server_add, 0, sizeof(server_add));
+    server_add.sin_family = AF_INET;
+    server_add.sin_port = htons(PORT);
+    /* Convert Internet host address from numbers-and-dots notation in CP
+   into binary data in network byte order.  */
+    unsigned long address;
+    address = inet_addr(ADDRESS);
+    // Memcopy the casted address
+    memcpy((char*) &server_add.sin_addr, &address, sizeof(address));
+    // BIND()
+    if (bind(fd_socket_server, (struct sockaddr*) &server_add, sizeof(server_add)) < 0) {
+        errorMessage("Could not bind to socket: ", strerror(errno), prog);
+    }
+    //fprintf(stdout, "Server Socket:%s:%d created.\n", server_add.sin_addr, ntohs(server_add.sin_port));
+    // LISTEN()
+    if (listen(fd_socket_server, BACKLOG) < 0) {
+        errorMessage("Could not listen to socket: ", strerror(errno), prog);
+    }
+    fprintf(stdout, "Server Socket created. Waiting ...\n");
+
+    // Endless loop, Server must be killed manually
+    socklen_t len_client = sizeof(client_add);
+    int rec_size;   // bytes received from client
+    while (1) {
+        fd_socket_client = accept(fd_socket_server, (struct sockaddr*) &client_add, &len_client);
+        if (fd_socket_client < 0) {
+            errorMessage("Could not accept socket: ", strerror(errno), prog);
+        }
+
+        rec_size = read(fd_socket_client, buffer, RECEIVERBUFFER);
+        if (rec_size < 0) {
+            errorMessage("Could not read from Client: ", strerror(errno), prog);
+        }
+        fprintf(stdout, "Client says: %s\n", buffer);
+
+        write(fd_socket_client, messageServer, strlen(messageServer));
+    }
+}
+
+/**
+ * Print out the error message for th user
+ * @param userMessage message appropriate for the user
+ * @param errorMessage message from the system
+ * @param progname provided from the args
+ */
+static void errorMessage(char* userMessage, char* errorMessage, const char* progname) {
+    fprintf(stderr, "%s: %s %s", progname, userMessage, errorMessage);
+    exit(EXIT_FAILURE);
+}
